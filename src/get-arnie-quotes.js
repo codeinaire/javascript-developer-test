30b9eda3da75ca298e86305afe0284a9de4c3ff4
John Stewart <john@asyncadventures.com>
2026-01-20 18:33:29 +1100

    update: ice to code you

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,30 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require('./mock-http-interface')
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+  const arnieQuotesPromises = urls.map((url) => httpGet(url))
+  const arnieQuotes = await Promise.all(arnieQuotesPromises)
+  return processArnieQuotes(arnieQuotes)
+}
+
+const SUCCESS_CODE = 200
+
+const processArnieQuotes = (arnieQuotes) => {
+  return arnieQuotes.map((arnieQuote) => {
+    const { status, body } = arnieQuote
+    const { message } = JSON.parse(body)
+
+    if (status === SUCCESS_CODE) {
+      return {
+        'Arnie Quote': message,
+      }
+    } else {
+      return {
+        FAILURE: message,
+      }
+    }
+  })
+}
 
 module.exports = {
   getArnieQuotes,
-};
+}
